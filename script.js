0be241418582201723c9eee0a6ab4faa7830f006
Logan Hemphill <logan.hemphill86@gmail.com>
2020-04-10 17:10:58 -0400

    build v0.2

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-// // API Key
-// b8f53b07d67511ae3702eb0275e02472
-
 $( document ).ready(function() {
 
     $("#search-button").on("click", function() {
@@ -30,7 +26,7 @@ $( document ).ready(function() {
             // Log the resulting object
             console.log(response);
 
-            console.log(response.weather[0].icon);
+            console.log(response.wind.speed);
     
             //   // Transfer content to HTML
             //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
@@ -44,13 +40,15 @@ $( document ).ready(function() {
             //   $(".temp").text("Temperature (K) " + response.main.temp);
             //   $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
     
-            //   // Log the data in the console as well
+            
             //   console.log("Wind Speed: " + response.wind.speed);
             //   console.log("Humidity: " + response.main.humidity);
             //   console.log("Temperature (F): " + tempF);
 
             iconcode = response.weather[0].icon;
             temp = response.main.temp;
+            humidity = response.main.humidity;
+            wind = response.wind.speed;
             
             var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=b8f53b07d67511ae3702eb0275e02472";
 
@@ -58,36 +56,17 @@ $( document ).ready(function() {
                 url: queryURL2,
                 method: "GET"
             })
-                // We store all of the retrieved data inside of an object called "response"
                 .then(function(response) {
         
-                // Log the queryURL
                 console.log(queryURL);
-        
-                // Log the resulting object
                 console.log(response);
-        
-                //   // Transfer content to HTML
-                //   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-                //   $(".wind").text("Wind Speed: " + response.wind.speed);
-                //   $(".humidity").text("Humidity: " + response.main.humidity);
-                
-                //   // Convert the temp to fahrenheit
-                //   var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        
-                //   // add temp content to html
-                //   $(".temp").text("Temperature (K) " + response.main.temp);
-                //   $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-        
-                //   // Log the data in the console as well
-                //   console.log("Wind Speed: " + response.wind.speed);
-                //   console.log("Humidity: " + response.main.humidity);
-                //   console.log("Temperature (F): " + tempF);
+                uvIndex = response.current.uvi;
+               
+                runCity();
             });
-            runCity();
-
+            
         });
-       
+        
     });
 
     function runCity() {
@@ -95,13 +74,19 @@ $( document ).ready(function() {
         $("#cityName").empty();
         $("#wicon").empty();
         $("#currDate").empty();
+        $("#temp").empty();
+        $("#humidity").empty();
+        $("#wind").empty();
+        $("#uvi").empty();
         $("#cityName").append($("#city").text());
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         $('#wicon').attr('src', iconurl);
-        $("#condIcon").append();
         $("#currDate").append(moment().format('LL'));
         // Stats
-        $("#temp").append(((temp - 273.15) * 1.80 + 32).toFixed(1) + " °F");
+        $("#temp").append("Temp : " + ((temp - 273.15) * 1.80 + 32).toFixed(1) + " °F");
+        $("#humidity").append("Humidity : " + humidity);
+        $("#wind").append("Wind : " + ((wind) * 2.237).toFixed(0) + " mph");
+        $("#uvi").append("UV : " + uvIndex);
 
     }
 });
